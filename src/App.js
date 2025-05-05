130a08fd6c837708573f1ac8627e57aabb58b02f
Aaron <115331438+Aaron2578@users.noreply.github.com>
2025-05-05 22:01:48 +0530

    initial commit
    First Commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h2>Guess the Number!</h2>;
+}
+function Body() {
+  let [value, setValue] = useState("");
+  let [count, setCount] = useState(10);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (count <= 0) return;
+    const randNumber = Math.floor(Math.random() * 10 + 1);
+    alert(`welcome ${value} ${randNumber}`);
+    setValue("");
+    if (randNumber === value) {
+      return alert("You Won! Keep Rocking!");
+    }
+    return alert("You Lose!");
+  };
+  function handleCount() {
+    setCount((prev) => (prev > 0 ? prev - 1 : prev));
+  }
+  return (
+    <div className="body">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter the Number {" from 1 to 10 : "}
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(+e.target.value)}
+          />
+        </label>
+        <input type="submit" onClick={handleCount} />
+      </form>
+      <h3>You have {count} change. Well Done !</h3>
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <footer>
+      <p className="para">
+        Build with{" "}
+        <span>
+          <img src="./logo192.png" alt="react icon" height="20px" />
+        </span>{" "}
+        React JS
+      </p>
+    </footer>
+  );
+}
